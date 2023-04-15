6db7411d785f6a77a6bbaafba290f63a77062bbb
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-15 10:26:17 -0600

    First versio ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'citas-medicas';
+  keyInfo= 'citas';
+
+  listCitas: any[] = [];
+
+  constructor(){
+
+  }
+
+  ngOnInit():void{
+    const info = localStorage.getItem(this.keyInfo);
+    if(!info){
+      localStorage.setItem(this.keyInfo,JSON.stringify([]))
+    }
+    else {
+      this.listCitas = JSON.parse(info);
+    }
+  }
+
+  agregarCita(cita:any){
+    this.listCitas.push(cita);
+    localStorage.setItem(this.keyInfo,JSON.stringify(this.listCitas));
+  }
+
+  eliminarCitaListado(index:number){
+    this.listCitas.splice(index,1);
+    localStorage.setItem(this.keyInfo,JSON.stringify(this.listCitas));
+  }
 }
