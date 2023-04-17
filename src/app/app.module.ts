81ce42950451c51dce7dfacae8212d614e7126e4
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-17 09:26:26 -0600

    Adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CrearCitasComponent } from './components/crear-citas/crear-citas.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearCitasComponent,
-    ListCitasComponent
+    ListCitasComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
